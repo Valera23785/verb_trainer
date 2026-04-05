7922811f8b818571b0e7ef22c2d4211a109d1e08
Valera <chengachgook@gmail.com>
2026-04-06 02:57:22 +0300

    fix new verbs counter in scheduler

--- core/scheduler.py
+++ core/scheduler.py
@@ -5,11 +5,13 @@ def get_todays_verbs(verbs: list[Verb], progress: UserProgress, config: dict) ->
     ''' Returns a list of verbs that are due for review today based on the user's progress and the configuration. '''
     
     today_verbs = []
+    new_verbs_added = 0
     for verb in verbs:
         verb_progress = progress.verbs.get(verb.id)
         if verb_progress is None:
-            if len(today_verbs) < config["daily_new_verbs"]:
+            if new_verbs_added < config["daily_new_verbs"]:
                 today_verbs.append(verb)
+                new_verbs_added += 1
         elif verb_progress.next_review <= date.today():
             today_verbs.append(verb)
     
