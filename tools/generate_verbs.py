709dc5d35a52a6c08bdad513bb1e24d8c57f3acf
Valera <chengachgook@gmail.com>
2026-04-19 23:56:45 +0300

    change tools/generate_verbs.py

--- tools/generate_verbs.py
+++ tools/generate_verbs.py
@@ -5,7 +5,7 @@ import logging
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
-file_handler = logging.FileHandler('generate_verbs.log', encoding='utf-8')
+file_handler = logging.FileHandler('tools/generate_verbs.log', encoding='utf-8')
 file_handler.setLevel(logging.DEBUG)
 formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
 file_handler.setFormatter(formatter)
