c28d9c5584f5f0b9d1565da624e4582bd5d9a37e
Valera <chengachgook@gmail.com>
2026-05-22 18:44:46 +0300

    add static files and dashboard styles

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI
 from api.dashboard import router as dashboard_router
+from fastapi.staticfiles import StaticFiles
 
 
 app = FastAPI()
-
 app.include_router(dashboard_router)
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 @app.get("/")
 def read_root():
